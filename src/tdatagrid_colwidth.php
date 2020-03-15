e22df238e23ccd79b3c9989db798d93e3442241f
Leonam, Carlos <carlosleonam@gmail.com>
2020-03-15 11:28:51 -0300

    Add TScript through PHP file (for __construct)

---
+++ src/tdatagrid_colwidth.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Created Date: Tuesday, October 22nd 2019, 7:48:20 am
+ * Author: Leonam, Carlos
+ *
+ * Copyright (c) 2019 SisSoftwaresWEB
+ */
+
+TScript::create("
+var arrOfTable = [],
+    i = 0;
+$('[id^=\"tdatagrid_\"] tr:nth-child(2) td').each(function () {
+    mWid = $(this).width();
+    arrOfTable.push(mWid);
+});
+var width_cols = escape(arrOfTable.join(','));
+document.cookie=\"profile_tdatagrid_". self::$formName ."_col_width = \" + width_cols;
+");
