80008beb0272e8950733c7392cb4c6876bf8e773
Leonam, Carlos <carlosleonam@gmail.com>
2020-03-15 13:45:58 -0300

    Update report buttons

--- src/ReportButtons.php
+++ src/ReportButtons.php
@@ -10,6 +10,6 @@
         $dropdown->setButtonClass('btn btn-default waves-effect dropdown-toggle');
         $dropdown->addAction( 'HTML', new TAction([$this, 'onGenerateHtml'], ['register_state' => 'false', 'static'=>'1']), 'fa:table #69aa46' );
         $dropdown->addAction( 'PDF', new TAction([$this, 'onGeneratePdf'], ['register_state' => 'false', 'static'=>'1']), 'far:file-pdf #d44734' );
-        $dropdown->addAction( 'RTF(doc)', new TAction([$this, 'onGenerateRtf'], ['register_state' => 'false', 'static'=>'1']), 'far:file-word #324bcc' );
+        $dropdown->addAction( 'RTF (doc)', new TAction([$this, 'onGenerateRtf'], ['register_state' => 'false', 'static'=>'1']), 'far:file-word #324bcc' );
         $dropdown->addAction( 'XLS', new TAction([$this, 'onGenerateXls'], ['register_state' => 'false', 'static'=>'1']), 'far:file-excel #cc7932' );
         $panel->addHeaderWidget( $dropdown );
